432d721a9f4c269d5bef5fbc4532f8d34c92e492
endur <endur@Naveen_PC>
2024-07-07 23:47:24 +0530

    Frist commit

---
+++ project1/src/com/tnsif/encapsulation/Demo.java
@@ -0,0 +1,15 @@
+package com.tnsif.encapsulation;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Encapsulationdemo e=new Encapsulationdemo();
+         e.serialnumber=1;
+         e.name="pinky";
+         e.age=22;
+         
+         e.show();
+	}
+
+}
---
+++ project1/src/com/tnsif/encapsulation/Encapsulationdemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.encapsulation; 
+
+public class Encapsulationdemo
+{
+
+		int serialnumber;
+		String name;
+		int age;
+		
+		void show() {
+			System.out.println("serialnumber"+ serialnumber +" " +"name" + name + " "+"age"+age);;
+		}
+     
+
+
+
+}
